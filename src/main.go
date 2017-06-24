1989659c5de4a499524cc02fd71ea7e7775a4e70
Vito Russo Moliterni <vito.russo.moliterni@gmail.com>
2017-06-24 22:21:16 +1000

    Renamed file

--- src/main.go
+++ src/main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/vitorussomoliterni/musichino/services/queryartist"
+	"github.com/vitorussomoliterni/musichino/services"
 )
 
 // Variables used for testing
 const artist string = "the men"
 
 func main() {
-	artistBestMatches := queryartist.GetArtist(artist)
+	artistBestMatches := services.queryartist.GetArtist(artist)
 	fmt.Println("Best matches found:")
 	for _, a := range artistBestMatches {
 		fmt.Println(a.friendlyString())
